4e1afe62a66f67e7483e966e36ffdd6282736bc7
fakin-mirna <117083548+fakin-mirna@users.noreply.github.com>
2023-03-22 12:39:27 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
-
-import './App.css';
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
-  return <div className="App">
-      Weather App
-    </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          Open-source code by{" "}
+          <a
+            href="https://www.linkedin.com/in/mirna-fakin/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fakin Mirna{" "}
+          </a>{" "}
+          on{" "}
+          <a
+            href="https://github.com/fakin-mirna/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
